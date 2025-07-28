fc86d762b1eb635860693c5e5b0f94a3b4ed5f53
codersintu <skmahto9199@gmail.com>
2025-07-28 17:17:26 +0530

    add more feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import video from "./assets/video1.mp4"
 
 import './App.css'
 import Navbar from './component/Navbar'
@@ -8,8 +9,12 @@ function App() {
   return (
     <>
      <Navbar/>
-    <div className="w-[100vw] h-[calc(100vh-70px)] flex flex-col justify-center items-center">
+    <div className="w-[100vw] flex flex-col justify-center items-center z-0">
     <Home/>
+    <div className="w-[1100px] mt-[80px]">
+      <video className='w-[100%]' src={video} autoPlay muted loop playsInline />
+    </div>
+
     </div>
     </>
   )
