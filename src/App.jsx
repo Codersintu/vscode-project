d2b50505cb38538774a19279a7a3e22d528057ca
codersintu <skmahto9199@gmail.com>
2025-07-28 18:30:04 +0530

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import video from "./assets/video1.mp4"
 import './App.css'
 import Navbar from './component/Navbar'
 import Home from './Home'
+import Card from './component/Card'
 
 function App() {
   return (
@@ -14,7 +15,11 @@ function App() {
     <div className="w-[1100px] mt-[80px]">
       <video className='w-[100%]' src={video} autoPlay muted loop playsInline />
     </div>
-
+    <div className="flex mt-[80px] gap-12">
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
     </div>
     </div>
   )
---
+++ src/component/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import gptimg from "../assets/gpt.png"
+
+function Card() {
+  return (
+<div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+<div className="flex flex-col gap-3">
+    <h1 className='text-xl font-semibold'>Any model for any team</h1>
+    <p className='text-gray-600 font-medium'>Use AI models like Claude Sonnet out of the box, or bring your own key to access models from Azure, Anthropic, Google, Ollama, OpenAI, and OpenRouter.</p>
+    <div className="">
+        <img src={gptimg} alt="" />
+    </div>
+</div>
+</div>
+  )
+}
+
+export default Card
