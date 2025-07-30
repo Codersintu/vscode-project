4cab7e8ea1bc00748d07bc0dd9fbe0c2bec22c1c
Codersintu <skmahto9199@gmail.com>
2025-07-30 21:10:47 +0530

    fix bug

--- src/component/Smallcard.jsx
+++ src/component/Smallcard.jsx
@@ -1,7 +1,7 @@
 
 import pyimg from '../assets/py.png'
 import Tyimg from "../assets/Ty.png"
-import javaimg from "../assets/java.png"
+// import javaimg from "../assets/java.png"
 import cimg from "../assets/c++.png"
 import cdimg from "../assets/dc.png"
 import GITimg from "../assets/Git.gif"
@@ -30,7 +30,7 @@ function Smallcard() {
                 </div>
                 <div class="max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                       <div className="flex justify-center items-center gap-3">
-                         <img className='mb-4' src={javaimg} alt="" />
+                         <img className='mb-4' src={pyimg} alt="" />
                         <div className="">
                             <h1 className='text-xl font-semibold'>Javascript</h1>
                             <p className='text-gray-500 text-sm'>Adds rich language support for python</p>
