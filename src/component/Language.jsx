cb2d521c18c02bb3f8f2dd901718db8eec0a37b7
Codersintu <skmahto9199@gmail.com>
2025-08-01 14:58:38 +0530

    fix bug

--- src/component/Language.jsx
+++ src/component/Language.jsx
@@ -22,8 +22,8 @@ const langImages = [
 ];
 function Language() {
   return (
-   <div className="w-full max-w-[1200px] mt-[70px] px-5 flex justify-center items-center">
-        <div className="flex flex-col md:flex-row ">
+   <div className="w-full max-w-[1300px] mt-[70px] px-5 flex justify-center items-center">
+        <div className="flex flex-col md:flex-row justify-center items-center">
            <div className="flex-1/4 flex flex-col gap-4">
              <h1 className='text-3xl font-semibold'>Code in any language</h1>
              <p className='mr-[130px] text-gray-600'>VS Code supports almost every major programming language. Several ship in the box, like JavaScript, TypeScript, CSS, and HTML, but extensions for others can be found in the VS Code Marketplace.</p>
